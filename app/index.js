8912e651428f0165df7908d0e94802d37bd3ed6b
Pramod Dhokane <Pramod Dhokane>
2016-09-27 14:44:13 +0530

    templates files

--- app/index.js
+++ app/index.js
@@ -1,17 +1,161 @@
 'use strict';
-var generators = require('yeoman-generator');
+var generators = require('yeoman-generator'),
+    _ = require('lodash'),
+    chalk = require('chalk'),
+    yosay = require('yosay');
 
 module.exports = generators.Base.extend({
     constructor: function(){
-        generators.Base.apply(this,arguments);
+        generators.Base.apply(this, arguments);
+        
+        this.argument('appname', { type: String, required: true });
+        this.appname = _.kebabCase(this.appname);
+        
+        this.option('includeutils', {
+           desc: 'Optionally includes Angular-UI Utils library.',
+           type: Boolean,
+           default: false 
+        });
     },
+    
     initializing: function(){
     },
     prompting: function(){
+        this.log(yosay('Welcome to ' + 
+            chalk.yellow('YANG (Yet Another Angular)') + ' generator!'));
+            
+            var done = this.async();
+            this.prompt([{
+                type: 'input',
+                name: 'ngappname',
+                message: 'Angular App Name (ng-app)',
+                default: 'app'
+            },
+            {
+                type: 'checkbox',
+                name: 'jslibs',
+                message: 'Which JS libraries would you like to include?',
+                choices: [
+                    {
+                        name: 'lodash',
+                        value: 'lodash',
+                        checked: true
+                    },
+                    {
+                        name: 'Moment.js',
+                        value: 'momentjs',
+                        checked: true
+                    },
+                    {
+                        name: 'Angular-UI Utils',
+                        value: 'angularuiutils',
+                        checked: true
+                    }
+                ]
+            }], function(answers){
+               this.log(answers);
+               this.ngappname = answers.ngappname;
+               this.includeLodash = _.includes(answers.jslibs, 'lodash');
+               this.includeMoment = _.includes(answers.jslibs, 'momentjs');
+               this.includeAngularUIUtils = _.includes(answers.jslibs, 'angularuiutils');               
+               done(); 
+            }.bind(this));
+            
     },
     configuring: function(){
     },
-    writing: function(){
+    writing: {
+        gulpfile: function(){
+            this.copy('_gulpfile.js', 'gulpfile.js');
+            this.copy('_gulp.config.js', 'gulp.config.js');
+            this.copy('jshintrc', '.jshintrc');
+        },
+
+        packageJSON: function(){
+            this.copy('_package.json', 'package.json');
+        },
+
+        git: function(){
+            this.copy('gitignore', '.gitignore');
+        },
+
+        bower: function(){
+            var bowerJson = {
+                name: this.appname,
+                license: 'MIT',
+                dependencies: {}  
+            };
+            bowerJson.dependencies['angular'] = '~1.4.6';
+            bowerJson.dependencies['angular-bootstrap'] = '~0.13.4';
+            bowerJson.dependencies['angular-ui-router'] = '~0.2.15';
+            bowerJson.dependencies['bootstrap-css-only'] = '~3.3.5';
+            if (this.includeLodash){
+                bowerJson.dependencies['lodash'] = '~3.10.1';                
+            }
+            if (this.includeMoment){
+                bowerJson.dependencies['moment'] = '~2.10.6';                
+            }
+            //if (this.options.includeutils){
+            if (this.includeAngularUIUtils){
+                bowerJson.dependencies['angular-ui-utils'] = '~3.0.0';
+            }
+            this.fs.writeJSON('bower.json', bowerJson);
+            
+            this.copy('bowerrc', '.bowerrc');
+        },
+
+        appStaticFiles: function(){
+            this.copy('_favicon.ico', 'src/favicon.ico');
+            this.directory('styles', 'src/styles');
+        },
+
+        scripts: function(){
+            this.fs.copyTpl(
+                this.templatePath('app/_app.js'),
+                this.destinationPath('src/app/app.js'),
+                {
+                    ngapp: this.ngappname
+                }
+            );
+            this.fs.copyTpl(
+                this.templatePath('app/layout/_shell.controller.js'),
+                this.destinationPath('src/app/layout/shell.controller.js'),
+                {
+                    ngapp: this.ngappname
+                });
+            this.fs.copyTpl(
+                this.templatePath('app/home/_home.controller.js'),
+                this.destinationPath('src/app/home/home.controller.js'),
+                {
+                    ngapp: this.ngappname
+                });
+            this.fs.copyTpl(
+                this.templatePath('app/about/_about.controller.js'),
+                this.destinationPath('src/app/about/about.controller.js'),
+                {
+                    ngapp: this.ngappname
+                });
+        },
+
+        html: function(){
+            this.fs.copyTpl(
+                this.templatePath('_index.html'),
+                this.destinationPath('src/index.html'),
+                {
+                    appname: _.startCase(this.appname),
+                    ngapp: this.ngappname
+                }
+            );
+            this.fs.copy(
+                this.templatePath('app/layout/_shell.html'),
+                this.destinationPath('src/app/layout/shell.html'));
+            this.fs.copy(
+                this.templatePath('app/home/_home.html'),
+                this.destinationPath('src/app/home/home.html'));
+            this.fs.copy(
+                this.templatePath('app/about/_about.html'),
+                this.destinationPath('src/app/about/about.html'));
+        }
     },
     conflicts: function(){
     },
