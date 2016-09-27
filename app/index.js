2f7b6fe0103354914c355260191c10ab7fa9457c
Pramod Dhokane <Pramod Dhokane>
2016-09-27 14:33:08 +0530

    yeoman index file changes

--- app/index.js
+++ app/index.js
@@ -1,9 +1,22 @@
 'use strict';
-
 var generators = require('yeoman-generator');
 
-module.exports= generators.Base.extend({
-    method1: function(){
-        this.log('Hello. World!');        
+module.exports = generators.Base.extend({
+    constructor: function(){
+        generators.Base.apply(this,arguments);
+    },
+    initializing: function(){
+    },
+    prompting: function(){
+    },
+    configuring: function(){
+    },
+    writing: function(){
+    },
+    conflicts: function(){
+    },
+    install: function(){
+    },
+    end: function(){
     }
 });
