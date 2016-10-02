cea70a29b581157336aa01104a372154459930df
Pramod Dhokane <Pramod Dhokane>
2016-10-02 18:23:30 +0530

    GulpTasks

--- app/index.js
+++ app/index.js
@@ -26,43 +26,6 @@ module.exports = generators.Base.extend({
                 message: 'Angular App Name (ng-app)',
                 default: this.config.get('ngappname') || 'app'
             },
-            // {
-            //     type: 'checkbox',
-            //     name: 'jslibs',
-            //     message: 'Which JS libraries would you like to include?',
-            //     choices: [
-            //         {
-            //             name: 'lodash',
-            //             value: 'lodash',
-            //             checked: true
-            //         },
-            //         {
-            //             name: 'Moment.js',
-            //             value: 'momentjs',
-            //             checked: true
-            //         },
-            //         {
-            //             name: 'Angular-UI Utils',
-            //             value: 'angularuiutils',
-            //             checked: true
-            //         },
-            //         {
-            //             name: 'angular-local-storage',
-            //             value: 'angularLocalStorage',
-            //             checked: true
-            //         },
-            //         {
-            //             name: 'angular-translate',
-            //             value: 'angularTranslate',
-            //             checked: true
-            //         },
-            //         {
-            //             name: 'angular-datatables',
-            //             value: 'angularDatatable',
-            //             checked: true
-            //         }
-            //     ]
-            // },
             {
                 type: 'checkbox',
                 name: 'csslibs',
@@ -88,12 +51,15 @@ module.exports = generators.Base.extend({
                 this.config.set('ngappname', answers.ngappname);
                 this.config.save();
 
-                this.includeLodash = _.includes(answers.jslibs, 'lodash');
-                this.includeMoment = _.includes(answers.jslibs, 'momentjs');
-                this.includeAngularUIUtils = _.includes(answers.jslibs, 'angularuiutils');
-                this.includeAngularLocalStorage = _.includes(answers.jslibs, 'angularLocalStorage');
-                this.includeAngularTranslate = _.includes(answers.jslibs, 'angularTranslate');
-                this.includeAngularDatatable = _.includes(answers.jslibs, 'angularDatatable');
+                // this.includeLodash = _.includes(answers.jslibs, 'lodash');
+                // this.includeMoment = _.includes(answers.jslibs, 'momentjs');
+                // this.includeAngularUIUtils = _.includes(answers.jslibs, 'angularuiutils');
+                // this.includeAngularLocalStorage = _.includes(answers.jslibs, 'angularLocalStorage');
+                // this.includeAngularTranslate = _.includes(answers.jslibs, 'angularTranslate');
+                // this.includeAngularDatatable = _.includes(answers.jslibs, 'angularDatatable');
+                
+                this.bootstrapTemplate = _.includes(answers.csslibs, 'bootstrap');
+                this.materialTemplate = _.includes(answers.csslibs, 'materialdesign');
                 this.installPackages = answers.installPackages;
                 
                 done();
@@ -131,36 +97,6 @@ module.exports = generators.Base.extend({
             this.copy('_bower.json', 'bower.json');
             this.copy('bowerrc', '.bowerrc');
             
-            // var bowerJson = {
-            //     name: this.config.get('ngappname'),
-            //     license: 'MIT',
-            //     dependencies: {}
-            // };
-            // bowerJson.dependencies['angular'] = '~1.4.6';
-            // bowerJson.dependencies['angular-bootstrap'] = '~0.13.4';
-            // bowerJson.dependencies['angular-ui-router'] = '~0.2.15';
-            // bowerJson.dependencies['bootstrap-css-only'] = '~3.3.5';
-            // bowerJson.dependencies['angular-mocks'] = '~1.4.7';
-            // if (this.includeLodash) {
-            //     bowerJson.dependencies['lodash'] = '~3.10.1';
-            // }
-            // if (this.includeMoment) {
-            //     bowerJson.dependencies['moment'] = '~2.10.6';
-            // }
-            // if (this.includeAngularUIUtils) {
-            //     bowerJson.dependencies['angular-ui-utils'] = '~3.0.0';
-            // }
-            // if (this.includeAngularLocalStorage) {
-            //     bowerJson.dependencies['angular-local-storage'] = '~0.2.3';
-            // }
-            // if (this.includeAngularTranslate) {
-            //     bowerJson.dependencies['angular-translate'] = '~2.8.1';
-            // }
-            // if (this.includeAngularDatatable) {
-            //     bowerJson.dependencies['angular-datatables'] = '~0.5.3';
-            // }
-            //this.fs.writeJSON('bower.json', bowerJson);
-          
         },
 
         appStaticFiles: function () {
@@ -184,6 +120,19 @@ module.exports = generators.Base.extend({
                 }
                 );
             this.fs.copyTpl(
+                this.templatePath('client/features/content/_content.controller.js'),
+                this.destinationPath('src/app/client/features/content/content.controller.js'),
+                {
+                    ngapp: this.config.get('ngappname')
+                });
+            this.fs.copyTpl(
+                this.templatePath('client/features/content/_routes.js'),
+                this.destinationPath('src/app/client/features/content/routes.js'),
+                {
+                    ngapp: this.config.get('ngappname')
+                });    
+                    
+            this.fs.copyTpl(
                 this.templatePath('client/features/footer/_footer.controller.js'),
                 this.destinationPath('src/app/client/features/footer/footer.controller.js'),
                 {
@@ -195,72 +144,72 @@ module.exports = generators.Base.extend({
                 {
                     ngapp: this.config.get('ngappname')
                 });
-            // this.fs.copyTpl(
-            //     this.templatePath('app/home/_home.controller.js'),
-            //     this.destinationPath('src/app/features/home/home.controller.js'),
-            //     {
-            //         ngapp: this.config.get('ngappname')
-            //     });
-            // this.fs.copyTpl(
-            //     this.templatePath('app/home/_home.config.js'),
-            //     this.destinationPath('src/app/features/home/config/home.config.js'),
-            //     {
-            //         ngapp: this.config.get('ngappname')
-            //     });
-            // this.fs.copyTpl(
-            //     this.templatePath('app/about/_about.controller.js'),
-            //     this.destinationPath('src/app/features/about/about.controller.js'),
-            //     {
-            //         ngapp: this.config.get('ngappname')
-            //     });
-            // this.fs.copyTpl(
-            //     this.templatePath('app/home/_routes.js'),
-            //     this.destinationPath('src/app/features/home/config/routes.js'),
-            //     {
-            //         ngapp: this.config.get('ngappname')
-            //     });
-            // this.fs.copyTpl(
-            //     this.templatePath('app/about/_routes.js'),
-            //     this.destinationPath('src/app/features/about/config/routes.js'),
-            //     {
-            //         ngapp: this.config.get('ngappname')
-            //     });
-            // this.fs.copyTpl(
-            //     this.templatePath('app/about/_about.config.js'),
-            //     this.destinationPath('src/app/features/about/config/about.config.js'),
-            //     {
-            //         ngapp: this.config.get('ngappname')
-            //     });
-            // this.fs.copyTpl(
-            //     this.templatePath('app/about/_about.unit.test.js'),
-            //     this.destinationPath('src/app/features/about/test/unit/about.unit.test.js'),
-            //     {
-            //         ngapp: this.config.get('ngappname')
-            //     });
-            // this.fs.copyTpl(
-            //     this.templatePath('app/login/_login.controller.js'),
-            //     this.destinationPath('src/app/features/login/login.controller.js'),
-            //     {
-            //         ngapp: this.config.get('ngappname')
-            //     });
-            // this.fs.copyTpl(
-            //     this.templatePath('app/login/_routes.js'),
-            //     this.destinationPath('src/app/features/login/config/routes.js'),
-            //     {
-            //         ngapp: this.config.get('ngappname')
-            //     });
-            // this.fs.copyTpl(
-            //     this.templatePath('app/_authService.js'),
-            //     this.destinationPath('src/app/services/authService.js'),
-            //     {
-            //         ngapp: this.config.get('ngappname')
-            //     });
-            // this.fs.copyTpl(
-            //     this.templatePath('app/_menuService.js'),
-            //     this.destinationPath('src/app/services/menuService.js'),
-            //     {
-            //         ngapp: this.config.get('ngappname')
-            //     });
+                
+            this.fs.copyTpl(
+                this.templatePath('client/features/header/_header.controller.js'),
+                this.destinationPath('src/app/client/features/header/header.controller.js'),
+                {
+                    ngapp: this.config.get('ngappname')
+                });
+            this.fs.copyTpl(
+                this.templatePath('client/features/header/_routes.js'),
+                this.destinationPath('src/app/client/features/header/routes.js'),
+                {
+                    ngapp: this.config.get('ngappname')
+                });
+                
+            this.fs.copyTpl(
+                this.templatePath('client/features/nav-bar/_navbar.controller.js'),
+                this.destinationPath('src/app/client/features/nav-bar/navbar.controller.js'),
+                {
+                    ngapp: this.config.get('ngappname')
+                });
+            this.fs.copyTpl(
+                this.templatePath('client/features/nav-bar/_routes.js'),
+                this.destinationPath('src/app/client/features/nav-bar/routes.js'),
+                {
+                    ngapp: this.config.get('ngappname')
+                });
+                
+            this.fs.copyTpl(
+                this.templatePath('client/features/quick-sidebar/_quick-sidebar.controller.js'),
+                this.destinationPath('src/app/client/features/quick-sidebar/quick-sidebar.controller.js'),
+                {
+                    ngapp: this.config.get('ngappname')
+                });
+            this.fs.copyTpl(
+                this.templatePath('client/features/quick-sidebar/_routes.js'),
+                this.destinationPath('src/app/client/features/quick-sidebar/routes.js'),
+                {
+                    ngapp: this.config.get('ngappname')
+                });
+                
+            this.fs.copyTpl(
+                this.templatePath('client/features/sidebar/_sidebar.controller.js'),
+                this.destinationPath('src/app/client/features/sidebar/sidebar.controller.js'),
+                {
+                    ngapp: this.config.get('ngappname')
+                });
+            this.fs.copyTpl(
+                this.templatePath('client/features/sidebar/_routes.js'),
+                this.destinationPath('src/app/client/features/sidebar/routes.js'),
+                {
+                    ngapp: this.config.get('ngappname')
+                });
+                
+            this.fs.copyTpl(
+                this.templatePath('client/features/theme-panel/_theme-panel.controller.js'),
+                this.destinationPath('src/app/client/features/theme-panel/theme-panel.controller.js'),
+                {
+                    ngapp: this.config.get('ngappname')
+                });
+            this.fs.copyTpl(
+                this.templatePath('client/features/theme-panel/_routes.js'),
+                this.destinationPath('src/app/client/features/theme-panel/routes.js'),
+                {
+                    ngapp: this.config.get('ngappname')
+                });
+            
             this.copy('_karma.conf.js', 'karma.conf.js');
             this.copy('_protractor-config.js', 'protractor-config.js');
            
